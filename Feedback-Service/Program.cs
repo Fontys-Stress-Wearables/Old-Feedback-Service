d14694078c41e881897038c7a5805eda82fb063b
jorickvn <jorickvannuland@hotmail.com>
2022-05-19 14:26:39 +0200

    - Updated heartbeat

--- Feedback-Service/Program.cs
+++ Feedback-Service/Program.cs
@@ -1,13 +1,17 @@
+using StressDataService.Nats;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<INatsService, NatsService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
